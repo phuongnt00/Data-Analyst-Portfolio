f0755e10f173b83016e4a55033b18e93f8e12112
Nguyễn Thị Phương <134161283+phuongnt00@users.noreply.github.com>
2023-12-15 00:44:38 +0700

    Update Online Retail.sql

--- Online Retail Cohort Analysis/Online Retail.sql
+++ Online Retail Cohort Analysis/Online Retail.sql
@@ -8,113 +8,116 @@ SELECT TOP (1000) [InvoiceNo]
       ,[Country]
   FROM [RetailOnline].[dbo].[Online_Retail]
 
-use RetailOnline
-go
+USE RetailOnline
+GO
 --- view whole dataset
-select *
-from Online_Retail
+SELECT *
+FROM Online_Retail
 
 --- cleaning dataset
-delete from Online_Retail
-where CustomerID is null
+DELETE FROM Online_Retail
+WHERE CustomerID IS NULL
 
-delete from Online_Retail
---select *
---from Online_Retail
-where 
-    Quantity < 0 or
+DELETE FROM Online_Retail
+--SELECT *
+--FROM Online_Retail
+WHERE 
+    Quantity < 0 OR
     UnitPrice < 0
 
 --- COHORT ANALYSIS ---
 -- create cohort date
-select 
+SELECT 
     CustomerID,
-    min(InvoiceDate) first_order_date,
+    MIN(InvoiceDate) first_order_date,
     DATEFROMPARTS(year(min(InvoiceDate)), month(min(InvoiceDate)), 1) cohort_date
-into #cohort_date
-from Online_Retail
-group by CustomerID
+INTO #cohort_date
+FROM Online_Retail
+GROUP BY CustomerID
 
-select * from #cohort_date
+SELECT * 
+FROM #cohort_date
 --- create cohort index
-select 
+SELECT 
     m.*,
     cohort_index=(order_year-cohort_year)*12 + (order_month-cohort_month) + 1
-into #cohort_retention 
-from   
+INTO #cohort_retention 
+FROM   
     (
-    select
+    SELECT
         o.*,
         c.cohort_date,
         month(c.cohort_date) cohort_month,
         year(c.cohort_date) cohort_year,
         month(o.InvoiceDate) order_month,
         year(o.InvoiceDate) order_year
-    from Online_Retail o
-    left join #cohort_date c
-        on o.CustomerID=c.CustomerID
+    FROM Online_Retail o
+    LEFT JOIN #cohort_date c
+        ON o.CustomerID=c.CustomerID
     )m
 
-select * from #cohort_retention
+SELECT * 
+FROM #cohort_retention
 
 --- Cohort retention by customer ---
-select
+SELECT
     cohort_date,
     cohort_index,
-    count (distinct CustomerID) Count_customer
-into #cohort_customer
-from #cohort_retention
-group by
+    COUNT (DISTINCT CustomerID) Count_customer
+INTO #cohort_customer
+FROM #cohort_retention
+GROUP BY
     cohort_date,
     cohort_index
-order by cohort_date
+ORDER BY cohort_date
 
-select * from #cohort_customer
+SELECT * 
+FROM #cohort_customer
 
 --pivot to see table
-select *
---into #piv_cohort_customer
-from #cohort_customer
-pivot
+SELECT *
+--INTO #piv_cohort_customer
+FROM #cohort_customer
+PIVOT
     (
-    sum(Count_customer)
-    for cohort_index in ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13])
+    SUM(Count_customer)
+    FOR cohort_index IN ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13])
     ) piv_cus
 
 --- Cohort Retention Rate ---
-select 
+SELECT 
     cohort_date,
-    round(1.0 * [1]/[1] *100, 2) as [1],
-    round(1.0 * [2]/[1] *100, 2) as [2],
-    round(1.0 * [3]/[1] *100, 2) as [3],
-    round(1.0 * [4]/[1] *100, 2) as [4],
-    round(1.0 * [5]/[1] *100, 2) as [5],
-    round(1.0 * [6]/[1] *100, 2) as [6],
-    round(1.0 * [7]/[1] *100, 2) as [7],
-    round(1.0 * [8]/[1] *100, 2) as [8],
-    round(1.0 * [9]/[1] *100, 2) as [9],
-    round(1.0 * [10]/[1] *100, 2) as [10],
-    round(1.0 * [11]/[1] *100, 2) as [11],
-    round(1.0 * [12]/[1] *100, 2) as [12],
-    round(1.0 * [13]/[1] *100, 2) as [13]
-from #piv_cohort_customer
+    round(1.0 * [1]/[1] *100, 2) AS [1],
+    round(1.0 * [2]/[1] *100, 2) AS [2],
+    round(1.0 * [3]/[1] *100, 2) AS [3],
+    round(1.0 * [4]/[1] *100, 2) AS [4],
+    round(1.0 * [5]/[1] *100, 2) AS [5],
+    round(1.0 * [6]/[1] *100, 2) AS [6],
+    round(1.0 * [7]/[1] *100, 2) AS [7],
+    round(1.0 * [8]/[1] *100, 2) AS [8],
+    round(1.0 * [9]/[1] *100, 2) AS [9],
+    round(1.0 * [10]/[1] *100, 2) AS [10],
+    round(1.0 * [11]/[1] *100, 2) AS [11],
+    round(1.0 * [12]/[1] *100, 2) AS [12],
+    round(1.0 * [13]/[1] *100, 2) AS [13]
+FROM #piv_cohort_customer
 
 --- Cohort retention by sales ---
-select
+SELECT
     cohort_date,
     cohort_index,
     round(sum(Quantity*UnitPrice), 2) Total_Sales
 --into #cohort_sales
-from #cohort_retention
-group by
+FROM #cohort_retention
+GROUP BY
     cohort_date,
     cohort_index
-order by cohort_date
+ORDER BY cohort_date
 --pivot to see table
-select *
-from #cohort_sales
-pivot
+SELECT *
+FROM #cohort_sales
+PIVOT
     (
     sum(Total_Sales)
-    for cohort_index in ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13])
+    FOR cohort_index IN ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13])
     ) piv_sales
